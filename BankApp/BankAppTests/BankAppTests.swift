4f63bf7f18540ac4b3246473416879bbedc02731
Aaron Lee <aaronlab.net@gmail.com>
2021-09-19 10:09:20 +0900

    deposit test

--- BankApp/BankAppTests/BankAppTests.swift
+++ BankApp/BankAppTests/BankAppTests.swift
@@ -9,13 +9,24 @@ import XCTest
 @testable import BankApp
 
 class BankAppTests: XCTestCase {
+    
+    private var account: Account!
+    
+    override func setUp() {
+        super.setUp()
+        
+        account = Account()
+    }
 
     func test_InitialBalanceZero() {
-        
-        let account = Account()
-        
         XCTAssertTrue(account.balance == 0, "Balance is not zero!")
+    }
+    
+    func test_DepositFunds() {
+        account.deposit(100)
         
+        XCTAssertEqual(account.balance, 100)
+        XCTAssertNotEqual(account.balance, 300)
     }
 
 }
