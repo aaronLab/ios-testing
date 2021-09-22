77ebe5d1174ad1284347d63ba7b75d1f8217f489
Aaron Lee <aaronlab.net@gmail.com>
2021-09-22 13:15:10 +0900

    init proj

---
+++ PedometerAppTests/PedometerAppTests/PedometerAppTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PedometerAppTestsApp.swift
+//  PedometerAppTests
+//
+//  Created by Aaron Lee on 2021/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PedometerAppTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
